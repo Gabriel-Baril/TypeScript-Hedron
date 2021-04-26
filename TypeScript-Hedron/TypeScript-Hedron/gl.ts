9d6f00338534abede0a9c163da95f1345770e1c7
Gabriel-Baril <gabriel-baril2010@hotmail.com>
2021-04-26 00:30:34 -0400

    Added WeBGL

---
+++ TypeScript-Hedron/TypeScript-Hedron/gl.ts
@@ -0,0 +1,34 @@
+﻿namespace Hedron {
+    /**
+     * The WegGL rendering context
+     * */
+    export var gl: WebGLRenderingContext; // Entry point of WebGL
+
+    /**
+     * Responsible for setting up a WebGL rendering context
+     */
+    export class GLUtilities {
+
+        public static init(elementId?: string): HTMLCanvasElement {
+            let canvas: HTMLCanvasElement;
+
+            if (elementId !== undefined) {
+                canvas = document.getElementById(elementId) as HTMLCanvasElement;
+                if (canvas === undefined) {
+                    throw new Error("Cannot find element with id: " + elementId);
+                }
+            } else {
+                canvas = document.createElement("canvas") as HTMLCanvasElement;
+                document.body.appendChild(canvas);
+            }
+
+            gl = canvas.getContext("webgl");
+            if (gl === undefined) {
+                throw new Error("Unable to initialize WebGL!");
+            }
+
+            return canvas;
+        }
+    }
+
+}
--- TypeScript-Hedron/TypeScript-Hedron/main.js
+++ TypeScript-Hedron/TypeScript-Hedron/main.js
@@ -1,23 +1,59 @@
+// The entry point of our application
+window.onload = function () {
+    var engine = new Hedron.Engine();
+    engine.start();
+};
 var Hedron;
 (function (Hedron) {
+    /**
+     * The main game engine class
+     * */
     var Engine = /** @class */ (function () {
         function Engine() {
             this._frameCount = 0;
         }
-        Engine.prototype.init = function () {
+        Engine.prototype.start = function () {
+            this._canvas = Hedron.GLUtilities.init("main-context");
+            Hedron.gl.clearColor(0, 0, 0, 1);
             this.loop();
         };
         Engine.prototype.loop = function () {
             this._frameCount++;
-            document.title = this._frameCount.toString();
+            Hedron.gl.clear(Hedron.gl.COLOR_BUFFER_BIT);
             requestAnimationFrame(this.loop.bind(this)); // Call this.loop on this specific instance to emulate an infinite loop
         };
         return Engine;
     }());
     Hedron.Engine = Engine;
 })(Hedron || (Hedron = {}));
-window.onload = function () {
-    var engine = new Hedron.Engine();
-    engine.init();
-};
+var Hedron;
+(function (Hedron) {
+    /**
+     * Responsible for setting up a WebGL rendering context
+     */
+    var GLUtilities = /** @class */ (function () {
+        function GLUtilities() {
+        }
+        GLUtilities.init = function (elementId) {
+            var canvas;
+            if (elementId !== undefined) {
+                canvas = document.getElementById(elementId);
+                if (canvas === undefined) {
+                    throw new Error("Cannot find element with id: " + elementId);
+                }
+            }
+            else {
+                canvas = document.createElement("canvas");
+                document.body.appendChild(canvas);
+            }
+            Hedron.gl = canvas.getContext("webgl");
+            if (Hedron.gl === undefined) {
+                throw new Error("Unable to initialize WebGL!");
+            }
+            return canvas;
+        };
+        return GLUtilities;
+    }());
+    Hedron.GLUtilities = GLUtilities;
+})(Hedron || (Hedron = {}));
 //# sourceMappingURL=main.js.map
